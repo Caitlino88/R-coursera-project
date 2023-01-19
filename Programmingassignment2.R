bc4b36f5ee5288000a42ffe652ea8333a304213f
Caitlino88 <113008703+Caitlino88@users.noreply.github.com>
2023-01-19 14:24:06 +0800

    Add files via upload

--- Programmingassignment2.R
+++ Programmingassignment2.R
@@ -1,3 +1,10 @@
+##1 Create a function makeVector() to create a special "vector", which is really a 
+##list containing a function to
+##-set the value of the mactrix
+##-get the value of the mactrix
+##-set the value of the inverse
+##-get the value of the inverse
+
 makeCacheMatrix <- function(x = matrix()) {
         s <- NULL
         set <- function(y){
@@ -12,7 +19,13 @@ makeCacheMatrix <- function(x = matrix()) {
              getsolve = getsolve)
 }
 
-cacheinverse <- function(x, ...){
+##2 Create a function to calculate the inverse of the special "vector" created 
+##with the above function. However, it first checks to see if the mean has already
+##been calculated. If so, it gets the inverse from the cache and skips the computation.
+##Otherwise, it calculates the inverse of the data and sets the value of the inverse in 
+##the cache via the setsolve function.
+
+cacheslove <- function(x, ...){
         s <- x$getsolve()
         if(!is.null(s)){
                 message("getting cached data")
